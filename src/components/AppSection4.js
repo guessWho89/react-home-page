43fed179b92e6cd1f0f7d8e88c78c504c8a0608f
Aco_M <aleksandarmilosevic89@gmail.com>
2020-11-08 11:52:33 +0100

    C:/Program Files/Git/

--- src/components/AppSection4.js
+++ src/components/AppSection4.js
@@ -9,7 +9,7 @@ export default function AppSection4() {
             <div className="grayBox">
                 <h2>Are you a Charity or Organisation looking to Fundraise?</h2>
                 <p>Want to register with AllGood.market so people can donate the proceeds of their sales to you? Please contact us at ifno@allgod.market or complete our online form.</p>
-                <Button class="btn">Sign up charity/campaign</Button>
+                <Button className="btn">Sign up charity/campaign</Button>
                 <img src={greenCircle} className="greenCircle "alt=""/>
             </div>
         </section>           
